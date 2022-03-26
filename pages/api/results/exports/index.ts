5ee1fb455bec9177fdb7359eae7b2c427101a708
matsnnik <matsnnik@fit.cvut.cz>
2022-03-26 17:17:05 +0100

    Added full logic for results and results/exports endpoints

--- pages/api/results/exports/index.ts
+++ pages/api/results/exports/index.ts
@@ -1,13 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {getResults} from "../../../components/logic/results"
+import { createReadStream, statSync } from 'fs'
+import { pipeline } from 'stream'
+const fs = require("fs");
+import path from 'path'
 
-type Data = {
-  name: string
-}
+const filePath = path.resolve('.', 'document.txt')
 
-export default function handler(
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  
+  const {
+    query: {  currency },
+    method,
+  } : NextApiRequest = req;
+  console.log(method);
+
+  let text = await getResults(currency.toString().toUpperCase());
+  console.log("result is " + text.toString());
+  var goodText : string = "";
+  for(let i = 0; i < text.length; i++){
+    goodText += text[i] + "\n";
+  }
+  try {
+    fs.writeFileSync("document.txt", goodText)
+    //file written successfully
+  } catch (err) {
+    console.error(err)
+  }
+  var filePath = path.join(__dirname, '../../../../../document.txt');
+  var stat = statSync(filePath);
+
+  res.writeHead(200, {
+      'Content-Type': 'txt/*',
+      'Content-Length': stat.size
+  });
+
+  var readStream = createReadStream(filePath);
+  // We replaced all the event handlers with a simple call to readStream.pipe()
+  await new Promise(function (resolve) {
+    readStream.pipe(res)
+    readStream.on('end', resolve)
+  })
+
 }
--- pages/components/logic/results.ts
+++ pages/components/logic/results.ts
@@ -9,30 +9,45 @@ type Currency = {
 
 export async function getResults(givenCurrencyName : string) {
 
+  let givenCurrency : string = givenCurrencyName;
+
+  if(givenCurrencyName === null || givenCurrencyName == ' '){
+    givenCurrency = 'CZK';
+  }
+
   let allCurrencies: string[] = getAllCurrencies();
+
+  if(!allCurrencies.includes(givenCurrency)){
+    return "Error!";
+  }
 
   let listOfCurrencies : Currency[] = [];
 
   for (let i = 0; i < allCurrencies.length - 1; i++) {
-
-    let currentExchangeValue: any = await getExchange(allCurrencies[i], givenCurrencyName);
-    console.log(currentExchangeValue);
-    if (isNaN(currentExchangeValue) || currentExchangeValue == 0) {
-
-        console.log(allCurrencies[i]);
-        console.log('Incorrect exchange value');
-        console.log(currentExchangeValue);
-        continue;
+    if(allCurrencies[i] != givenCurrency){
+      let currentExchangeValue: any = await getExchange(allCurrencies[i], givenCurrency);
+      console.log(currentExchangeValue);
+      if (isNaN(currentExchangeValue) || currentExchangeValue == 0) {
+  
+          console.log(allCurrencies[i]);
+          console.log('Incorrect exchange value');
+          console.log(currentExchangeValue);
+          continue;
+      }
+  
+      let currentCurrency : Currency = { name : allCurrencies[i], value : currentExchangeValue};
+      listOfCurrencies.push(currentCurrency);
     }
-
-    let currentCurrency : Currency = { name : allCurrencies[i], value : currentExchangeValue};
-    listOfCurrencies.push(currentCurrency);
   }
   
   listOfCurrencies.sort((a, b) => (a.value > b.value ? -1 : 1));
   let currenciesSortedString : string[] = []; 
   for(let i = 0; i < listOfCurrencies.length - 1; i++){
-    currenciesSortedString.push((i + 1) + ". 1 " + listOfCurrencies[i].name + " -> " + listOfCurrencies[i].value + " " + givenCurrencyName);
+    if(i != listOfCurrencies.length - 2){
+      currenciesSortedString.push((i + 1) + ". 1 " + listOfCurrencies[i].name + " -> " + listOfCurrencies[i].value + " " + givenCurrency + ",");
+    }else{
+      currenciesSortedString.push((i + 1) + ". 1 " + listOfCurrencies[i].name + " -> " + listOfCurrencies[i].value + " " + givenCurrency);
+    }
   }
 return currenciesSortedString;
 }
